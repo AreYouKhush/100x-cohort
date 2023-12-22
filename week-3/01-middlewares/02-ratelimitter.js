b185346a4b727ad4a0b02cbccdd94a9a25ee88a6
Khushal Suthar <khushal9587@gmail.com>
2023-12-22 23:27:09 +0530

    week 3

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 const app = express();
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
@@ -11,17 +11,39 @@ const app = express();
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
 
+let requests = 1;
 let numberOfRequestsForUser = {};
-setInterval(() => {
+
+function rateLimit(req, res, next) {
+  const userId = req.header["user-id"];
+  setInterval(() => {
     numberOfRequestsForUser = {};
-}, 1000)
+  }, 1000);
+  numberOfRequestsForUser = {
+    user: userId,
+    requests: requests++,
+  };
+  if (numberOfRequestsForUser.requests >= 5) {
+    requests = 1;
+    return res.status(404).json({
+      msg: "Nothing"
+    });
+  } else {
+    next();
+  }
+}
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+app.use(rateLimit);
+
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john", request: numberOfRequestsForUser.requests });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
+
+app.listen(3000)
 
 module.exports = app;
